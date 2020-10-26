61c9687695655a3d14d03b88acd812e66b6768bc
Ashwin Km <kmuraliashwin@gmail.com>
2020-10-26 20:28:14 +0530

    Converted repeated FormElement into Array

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,17 @@
 import React from "react";
+import FormElement from "./FormElement";
 
 const Form = () => {
-  return <section className="Form">Form</section>;
+  return (
+    <section className="Form">
+      <h2>Enter your details</h2>
+      <form>
+        {["Name", "Designation", "Phone", "Email"].map((field, key) => (
+          <FormElement ID={field} Label={field} key={key} />
+        ))}
+      </form>
+    </section>
+  );
 };
 
 export default Form;
